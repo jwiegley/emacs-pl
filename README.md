b34acf76712c42853babec7d2e213e14c848fff7
John Wiegley <johnw@newartisans.com>
2015-03-19 05:17:57 -0500

    Initial version

---
+++ README.md
@@ -0,0 +1,64 @@
+This is a parsing library in the spirit of Haskell's parsec.  For example:
+
+``` elisp
+(pl-parse
+  (delete-region (pl-str "<xml>" :beg)
+                 (progn
+                   (pl-anything)
+                   (pl-str "</xml>" :end))))
+```
+
+There are a few parsers, whose job is to inspect whatever is at the current
+buffer position, and return zero or more details regarding what was found:
+
+    pl-ch                Match a single character
+    pl-str               Match a string
+    pl-re                Match a regular expression
+    pl-num               Match an integer or floating-point number
+
+Other possibilities include: inspecting text propertis, overlays, etc.
+
+If the parser succeeds, it returns the object matched (a string by default),
+and advances point to the next position after the match.  Keywords may be
+given to return other details:
+
+    :beg                 Beginning of the match
+    :end                 End of the match
+    :group N             A particular regexp group
+    :props               All properties within the matched region
+    :nil                 Return `nil` (same as using `ignore`)
+
+If a parser fails, it throws the exception `failed`.  This is caught by the
+macro `pl-try`, which returns `nil` upon encountering the exception.  This
+makes it possible to build certain combinators out of these few parts:
+
+    pl-or                Return result from first successful parser
+    pl-and               Return last result, if all parsers succeed
+    pl-until             If the parse fails, advance cursor position by
+                         one character and try again.  Keywords can
+                         change the advance amount.
+
+For other constructs, like return the result of every parser as a list, just
+combine parsers with regular Lisp forms (`pl-parse` is just a synonym for
+`pl-try`):
+
+``` elisp
+(pl-parse
+  (list (pl-str "Hello") (pl-str "World")))
+```
+
+Note that even though a parse may fail, and thus return no value, any
+side-effects that occur during the course of the parse will of course be
+retained.  This can be used to good effect, by continuing an action for as
+long as a parse succeeds:
+
+``` elisp
+(pl-parse
+  (while t
+     (delete-region (pl-str "<xml>" :beg)
+                    (pl-until
+                      (pl-str "</xml>" :end)))))
+```
+
+This will delete blocks demarcated by `<xml>` and `</xml>`, for as long as
+such blocks continue to occur contiguously to one another.
---
+++ pl.el
@@ -0,0 +1,112 @@
+;;; pl --- Combinator parsing library for Emacs, similar to Haskell's Parsec
+
+;; Copyright (C) 2015 John Wiegley
+
+;; Author: John Wiegley <jwiegley@gmail.com>
+;; Created: 19 Mar 2015
+;; Version: 1.0
+;; Keywords: parsing lisp devel
+;; X-URL: https://github.com/jwiegley/emacs-pl
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;; Please see the README
+
+(eval-when-compile
+  (require 'cl))
+(require 'cl-lib)
+
+(defgroup pl nil
+  "Combinator parsing library for Emacs, similar to Haskell's Parsec"
+  :group 'development)
+
+(defmacro pl-try (&rest forms)
+  `(catch 'failed ,@forms))
+
+(defalias 'pl-parse 'pl-try)
+
+(defun pl-ch (ch &rest args)
+  (if (char-equal (char-after) ch)
+      (prog1
+          (cond
+           ((memq :nil args) nil)
+           ((memq :beg args)
+            (point))
+           ((memq :end args)
+            (1+ (point)))
+           (t
+            (char-to-string ch)))
+        (forward-char 1))
+    (throw 'failed nil)))
+
+(defun pl-re (regexp &rest args)
+  (if (looking-at regexp)
+      (prog1
+          (cond
+           ((memq :nil args) nil)
+           ((memq :beg args)
+            (match-beginning 0))
+           ((memq :end args)
+            (match-end 0))
+           ((memq :group args)
+            (let ((group
+                   (loop named outer for arg on args
+                         when (eq (car arg) :group) do
+                         (return-from outer (cadr arg)))))
+              (if group
+                  (match-string group)
+                (error "Unexpected regexp :group %s" group))))
+           (t
+            (match-string 0)))
+        (goto-char (match-end 0)))
+    (throw 'failed nil)))
+
+(defsubst pl-str (str &rest args)
+  (pl-re (regexp-quote str)))
+
+(defsubst pl-num (num &rest args)
+  (pl-re (regexp-quote (number-to-string num))))
+
+(defmacro pl-or (&rest parsers)
+  (let ((outer-sym (make-symbol "outer"))
+        (parser-sym (make-symbol "parser")))
+    `(loop named ,outer-sym for ,parser-sym in ',parsers
+           finally (throw 'failed nil) do
+           (catch 'failed
+             (return-from ,outer-sym (eval ,parser-sym))))))
+
+(defmacro pl-and (&rest parsers)
+  (let ((result-sym (make-symbol "result"))
+        (parser-sym (make-symbol "parser")))
+    `(let (,result-sym)
+       (loop for ,parser-sym in ',parsers do
+             (setq ,result-sym (eval ,parser-sym)))
+       ,result-sym)))
+
+(defmacro pl-until (parser &optional &key skip)
+  `(catch 'done
+     (while (not (eobp))
+       (catch 'failed
+         (throw 'done ,parser))
+       ,(if skip
+            `(,skip 1)
+          `(forward-char 1)))))
+
+(provide 'pl)
+
+;;; pl.el ends here
